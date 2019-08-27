c8d53b2c7bfeee67e65bfaa94474b50670b1e0bf
desacu <noemidsc@gmail.com>
2019-08-27 12:40:11 +0100

    fix test api

--- src/test/kotlin/es/leanmind/boilerplace/integration/ExampleApiControllerShould.kt
+++ src/test/kotlin/es/leanmind/boilerplace/integration/ExampleApiControllerShould.kt
@@ -5,8 +5,8 @@ package es.leanmind.boilerplace.integration
 import es.leanmind.boilerplace.domain.models.Pet
 import es.leanmind.boilerplace.domain.models.User
 import es.leanmind.boilerplace.domain.services.UserService
-import es.leanmind.boilerplace.infrastructure.dto.UserDTO
 import es.leanmind.boilerplace.interfaces.ApiIntegrationTest
+import org.json.JSONArray
 import org.json.JSONObject
 import org.junit.Test
 import org.junit.experimental.categories.Category
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
@@ -45,16 +44,19 @@ class ExampleApiControllerShould {
 
     @Test
     fun parse_retrieved_user_to_json() {
-        val expectedPatient = JSONObject()
-        expectedPatient.put("id", user.id)
-        expectedPatient.put("name", user.name)
-        expectedPatient.put("pets", pets.map { pet -> pet.name })
+        val expectedPets = JSONArray()
+        pets.map { pet -> expectedPets.put(pet.name) }
+
+        val expectedUser = JSONObject()
+        expectedUser.put("id", user.id)
+        expectedUser.put("name", user.name)
+        expectedUser.put("pets", expectedPets)
 
         given(userServiceStub.getUserById(user.id)).willReturn(user)
 
         mvc.perform(get("/api/users/" + user.id))
                 .andExpect(status().isOk)
-                .andExpect(content().json("[" + expectedPatient.toString() + "]"))
+                .andExpect(content().json(expectedUser.toString()))
     }
 
 }
